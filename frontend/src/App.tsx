f60cf62da51eca0a2c957a53f3f7eacd64c7c6f0
Philip Bartmann <p.bartmann@oth-aw.de>
2023-02-23 16:25:03 +0100

    Database Access from frontend for dev and prod

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,18 +2,25 @@ import { Button } from "@mui/material";
 import { useState } from "react";
 
 export function App() {
-    const [count, setCount] = useState<number>(1);
+    const [entry, setEntry] = useState<string>("");
     const env = import.meta.env;
 
     function handleClick() {
-        setCount((count) => count + 1);
+        fetch(import.meta.env.VITE_BACKEND_URL + "test.php", {
+            body: undefined,
+            method: "GET",
+            credentials: "include",
+        })
+            .then((response) => response.json())
+            .then((value) => setEntry(value));
     }
 
     return (
         <>
             <h1>Hello World</h1>
             {JSON.stringify(env)}
-            <Button onClick={handleClick}>{count}</Button>
+            <Button onClick={handleClick}>Get Database</Button>
+            <div>{JSON.stringify(entry)}</div>
         </>
     );
 }
